2e139b498d0b1847701892873f7ba1b03551e2b6
Nicolas Takashi <nicolas.tcs@hotmail.com>
2021-10-18 21:03:02 +0100

    [TEST] include dashboard loader test

---
+++ internal/dashboardloader/dashboardloader_test.go
@@ -0,0 +1,41 @@
+package dashboardloader
+
+import (
+	"testing"
+)
+
+func TestLoadInvalidFile(t *testing.T) {
+	dashboards, err := Load("./testdata/invalid")
+
+	if err == nil {
+		t.Fatal("load an invalid json file must return an error")
+	}
+
+	if len(dashboards) > 0 {
+		t.Fatal("load an invalid json file must not return a dashboard struct")
+	}
+}
+
+func TestLoadValidFile(t *testing.T) {
+	dashboards, err := Load("./testdata/valid")
+
+	if err != nil {
+		t.Fatal("load an valid json file must not return an error")
+	}
+
+	if len(dashboards) == 0 {
+		t.Fatal("load an valid json file must return a dashboard struct")
+	}
+}
+
+func TestLoadEmptyFile(t *testing.T) {
+	dashboards, err := Load("./testdata/empty")
+
+	if err == nil {
+		t.Fatal("load an empty json file must return an error")
+	}
+
+	if len(dashboards) > 0 {
+		t.Fatal("load an empty json file must not return a dashboard struct")
+	}
+}
